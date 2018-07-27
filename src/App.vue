d4fb7895686fa21aa7415b59648a47d0970c6539
eloylp <e.lopez@xeitodevs.com>
2018-07-27 13:58:31 +0200

    adding vuetify, and sample app layout. unstable commit.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,91 @@
 <template>
-  <div class="container center-content">
-    <router-view/>
-  </div>
+  <v-app
+    id="pakistrano-camera-viewer"
+    dark
+  >
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="Number()">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="Number()">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+      fixed
+      clipped-left
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container
+        fluid
+        fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+          <v-flex text-xs-center>
+            <v-tooltip left>
+              <v-btn
+                slot="activator"
+                :href="source"
+                icon
+                large
+                target="_blank"
+              >
+                <v-icon large>code</v-icon>
+              </v-btn>
+              <span>Source</span>
+            </v-tooltip>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer
+      app
+      fixed
+    >
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+  </v-app>
+  <!--<div class="container center-content">-->
+  <!--<router-view/>-->
+  <!--</div>-->
 </template>
 
-<style scoped>
+<script>
 
+export default {
+  props: {
+    drawer: {
+      default: null,
+      type: Boolean
+    },
+    source: {
+      default: null,
+      type: Boolean
+    }
+  }
+}
+</script>
+<style scoped>
 </style>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import router from './router'
 import App from './App'
 
 import 'assets/css/app.styl'
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VBtn,
+  VIcon,
+  VList,
+  VToolbar,
+  VTooltip,
+  VFooter,
+  VLayout,
+  VListTile,
+  VListTileAction,
+  VContent,
+  VContainer,
+  VFlex
+} from 'vuetify'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VIcon,
+    VBtn,
+    VList,
+    VLayout,
+    VListTile,
+    VListTileAction,
+    VToolbar,
+    VContent,
+    VContainer,
+    VFlex,
+    VTooltip,
+    VFooter
+  }
+})
 
 /* eslint-disable-next-line no-new */
 new Vue({
